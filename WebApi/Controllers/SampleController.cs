618e2440da778000edf57f1e400a7a274198800f
parmar vrutik <parmarvrutik763@gmail.com>
2024-09-08 12:56:40 +0530

    Add project files.

---
+++ WebApi/Controllers/SampleController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly List<string> sampleData = new List<string>
+        {
+            "Item 1",
+            "Item 2",
+            "Item 3"
+        };
+
+        // GET: api/sample
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(sampleData);
+        }
+
+        // GET: api/sample/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            if (id >= 0 && id < sampleData.Count)
+            {
+                return Ok(sampleData[id]);
+            }
+
+            return NotFound("Item not found");
+        }
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World..!");
+                });
+            });
+        }
+    }
+}
